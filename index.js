87f453342bbef73068c636e07fa5cc7de5bc735f
George Agathos <gagathos@thecommotion.com>
2017-12-30 12:31:46 -0500

    Added command line options and logging

--- index.js
+++ index.js
@@ -5,27 +5,34 @@
 
 const IOTA = require('iota.lib.js')
 const exec = require('child_process').exec;
+const argv = require('minimist')(process.argv.slice(2));
+const fs = require('fs')
+const newline = require('os').EOL;
+const dateFormat = require('dateformat')
+
+var log_file = fs.createWriteStream('watchdog.log', {flags : 'a'});
 
 //This is meant to only run once via cron
 
 const threshold = 20 //number of milestones behind we can be
 const protocol = 'http'
-const host = '127.0.0.1'
-const port = 14625
+const host = argv.h ? argv.h : '127.0.0.1'
+const port = argv.p ? argv.p : 14625
+
 const resyncing_milestone = 243000
-const restart_iri_command = "service iota restart"
+const restart_iri_command = argv.c ? arv.c : "service iota restart"
 
 var iota = new IOTA({host:protocol + '://'+ host,port:port})
 
 iota.api.getNodeInfo(function(error, success){
 	if(success){
 		if(success.latestSolidSubtangleMilestoneIndex < success.latestMilestoneIndex - threshold){
-				console.log('Node is out of sync!!!')
+				log('Node is out of sync!!!')
 				restartIRI()
 			} else if(success.latestMilestoneIndex == resyncing_milestone) {
-				console.log('Node is resyncing')
+				log('Node Resyncing')
 			} else {
-				console.log('Node seems OK')
+				log('Node OK')
 			}
 	} else {
 		console.log(error)
@@ -33,16 +40,19 @@ iota.api.getNodeInfo(function(error, success){
 })
 
 function restartIRI(){
-	
+	log('Restarting IRI')
 	exec(restart_iri_command, (err, stdout, stderr) => {
 	  if (err) {
 	    // node couldn't execute the command
-		  console.log('error issuing command')
+		 log('Some error happened restarting iri '+err)
 	    return;
 	  }
 
-	  // the *entire* stdout and stderr (buffered)
-	  console.log(`stdout: ${stdout}`);
-	  console.log(`stderr: ${stderr}`);
 	});
+}
+
+function log(message){
+	console.log(message)
+	let timestamp = dateFormat(new Date(), 'yyyy-mm-dd hh:MM:ss')
+	log_file.write(timestamp + ' - ' + message + newline)
 }
